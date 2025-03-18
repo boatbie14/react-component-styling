4f22123ecebf17fb77e70e24d435bf9d96ea3256
Kittiphat <kittiphat.rpp@gmail.com>
2025-03-18 13:36:27 +0700

    feat/exercise1

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,35 @@
-// Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Alert() {}
+function Alert({ type, text }) {
+  const errorStyle = css`
+    background-color: red;
+  `;
+  const warningStyle = css`
+    background-color: orange;
+  `;
+  const infoStyle = css`
+    background-color: yellow;
+  `;
+  const successStyle = css`
+    background-color: green;
+  `;
+
+  const checkAlertType = (type) => {
+    if (type === "error") {
+      return errorStyle;
+    } else if (type === "warning") {
+      return warningStyle;
+    } else if (type === "info") {
+      return infoStyle;
+    } else if (type === "success") {
+      return successStyle;
+    } else {
+      return null;
+    }
+  };
+
+  return <div css={checkAlertType(type)}>This is {text} alert box.</div>;
+}
+
+export default Alert;
